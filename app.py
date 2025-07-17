ca6f45996d9183ac3331d934b8143e67bf943fe5
SahibTaj <sahibtajsingh1@gmail.com>
2025-07-18 02:15:27 +0530

    Updated app.py and .gitignore

--- app.py
+++ app.py
@@ -2,31 +2,23 @@ from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 
-# Load the trained model
 model = joblib.load('models/best_model.pkl')  # ✅ Path to your trained model
 
-# Create Flask app
 app = Flask(__name__)
 
-# Home route (optional)
 @app.route('/')
 def home():
     return "🚀 Fraud Detection API is running!"
 
-# Prediction route
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Get JSON data from request
         input_data = request.json
         
-        # Extract features (V1–V28, Amount)
         features = np.array([input_data['features']]).reshape(1, -1)
 
-        # ✅ Get fraud probability
         proba = model.predict_proba(features)[0][1]  # class 1 = fraud
 
-        # ✅ Optional: use threshold (0.5) to decide label
         threshold = 0.5
         prediction = "Fraud" if proba > threshold else "Not Fraud"
 
